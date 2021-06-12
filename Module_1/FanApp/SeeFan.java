1aa453fe01ff30141d0b45731d4cfab6b9f08c86
Naomi See <naomi.see@mutualofomaha.com>
2021-06-12 15:40:27 -0500

    Committing skeleton Fan app because I'm a safety first sort of gal.

---
+++ Module_1/FanApp/SeeFan.java
@@ -0,0 +1,11 @@
+package Module_1.FanApp;
+
+/*
+    Liang, Y.D. (2019). Introduction to Java Programming and Data Structures: 
+        Comprehensive Version (12th ed.). Pearson Education, Inc.
+    Modified by N. See 2021
+*/ 
+
+public class SeeFan {
+    
+}
