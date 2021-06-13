59e033a1e86c31051406c6a8d76f99d7b2fd8e35
Naomi See <naomi.see@mutualofomaha.com>
2021-06-13 17:36:54 -0500

    Pushing up completed Fan app with passing tests

--- Module_1/FanApp/SeeFan.java
+++ Module_1/FanApp/SeeFan.java
@@ -7,29 +7,15 @@ package Module_1.FanApp;
 
 public class SeeFan {
     
-    final int STOPPED = 0;
-    final int SLOW = 1;
-    final int MEDIUM = 2;
-    final int FAST = 3;
+    final static int STOPPED = 0;
+    final static int SLOW = 1;
+    final static int MEDIUM = 2;
+    final static int FAST = 3;
 
     private int speed;
     private Boolean on;
-    private double radius;
+    private int radius;
     private String color;
-
-    public SeeFan() {
-        speed = 0;
-        on = false;
-        radius = 6;
-        color = "white";
-    }
-
-    public SeeFan(int speed, Boolean on, double radius, String color) {
-        this.speed = speed;
-        this.on = on;
-        this.radius = radius;
-        this.color = color;
-    }
 
     public int getSpeed() {
         return speed;
@@ -46,7 +32,7 @@ public class SeeFan {
     public double getRadius() {
         return radius;
     }
-    public void setRadius(double radius) {
+    public void setRadius(int radius) {
         this.radius = radius;
     }
     public String getColor() {
@@ -56,4 +42,54 @@ public class SeeFan {
         this.color = color;
     }
 
-}
+    public SeeFan() {
+        speed = STOPPED;
+        on = false;
+        radius = 6;
+        color = "white";
+    }
+
+    public SeeFan(int speed, Boolean on, int radius, String color) {
+        this.speed = speed;
+        this.on = on;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    /**
+     * Method with four arguments, which are used to print the fan details and whether its on or off.
+     * @param speed int 
+     * @param on Boolean
+     * @param radius int
+     * @param color string
+     * @return prints the fan details to the console window.
+     */
+
+    public String toString() { 
+        if(on) {
+            return "\nOh hello. \nLooks like the fan is currently on, moving at a speed of " + this.speed + " hundred rpms. \nThe fan's blade radius is: " + this.radius + " inches. \nThe fan's color is: " + this.color +  ".\n";
+        } else {
+            this.speed = STOPPED;
+            return "\nGood day. \nLooks like the fan is currently off, resulting in a speed of " + this.speed + " rpms. \nThe fan's blade radius is: " + this.radius + " inches. \nThe fan's color is: " + this.color +  ".\n";
+        }
+    }// end toString method
+
+    public static void main(String[] args) {
+        // below test should print out default set values set in the default constructor
+        SeeFan f1 = new SeeFan();
+
+        // below test should print the updated values set and passed to the argument constructor
+        SeeFan f2 = new SeeFan(MEDIUM, true, 15, "purple");
+        
+        /** 
+         * below test should print the updated values set and passed to the argument constructor
+         * below test should also reset speed to STOPPED regardless of speed value passed if fan is off 
+         */
+        SeeFan f3 = new SeeFan(FAST, false, 3, "green");
+        
+
+        System.out.println(f1); //compiler prints f1.toString
+        System.out.println(f2); //compiler prints f2.toString
+        System.out.println(f3); //compiler prints f3.toString
+    }// end main
+}// end SeeFan
