47bce9779cb5866b8ec601c2e74964c34c2e507c
Naomi See <naomi.see@mutualofomaha.com>
2021-06-13 13:02:26 -0500

    Building out the Fan app adding more structure within the class. Pushing up to access from another computer.

--- Module_1/FanApp/SeeFan.java
+++ Module_1/FanApp/SeeFan.java
@@ -1,5 +1,4 @@
 package Module_1.FanApp;
-
 /*
     Liang, Y.D. (2019). Introduction to Java Programming and Data Structures: 
         Comprehensive Version (12th ed.). Pearson Education, Inc.
@@ -8,4 +7,53 @@ package Module_1.FanApp;
 
 public class SeeFan {
     
+    final int STOPPED = 0;
+    final int SLOW = 1;
+    final int MEDIUM = 2;
+    final int FAST = 3;
+
+    private int speed;
+    private Boolean on;
+    private double radius;
+    private String color;
+
+    public SeeFan() {
+        speed = 0;
+        on = false;
+        radius = 6;
+        color = "white";
+    }
+
+    public SeeFan(int speed, Boolean on, double radius, String color) {
+        this.speed = speed;
+        this.on = on;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+    public Boolean getOn() {
+        return on;
+    }
+    public void setOn(Boolean on) {
+        this.on = on;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+
 }
