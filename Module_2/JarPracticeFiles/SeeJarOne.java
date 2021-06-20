4bd9d113c0ace9a112bf68c47ec6e3f30d5b0a5b
Naomi See <naomi.see@mutualofomaha.com>
2021-06-19 20:40:40 -0500

    Adding commenting to the java files just incase I am expected to.

--- Module_2/JarPracticeFiles/SeeJarOne.java
+++ Module_2/JarPracticeFiles/SeeJarOne.java
@@ -1,5 +1,10 @@
 package Module_2.JarPracticeFiles;
+/* 
+See, Naomi. (2021). CIS 505 Intermediate Java Programming.
+Bellevue University. 
+*/
 
+// The purpose of this shell file is purely to demonstrate combining JarOne & JarTwo java files into a single jar file.
 public class SeeJarOne {
     
 }
